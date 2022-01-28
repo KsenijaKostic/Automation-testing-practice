8416ac08ba1df00164979f638e7324c8895bb3d6
KsenijaKostic <96876954+KsenijaKostic@users.noreply.github.com>
2022-01-28 13:51:23 +0100

    Add files via upload

---
+++ ProjectPractice/Tests/SidebarTest.java
@@ -0,0 +1,19 @@
+package ProjectPractice.Tests;
+
+import ProjectPractice.Base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SidebarTest extends BasePage {
+
+    @BeforeMethod
+    public void setUpPage(){
+        //maximizujem
+        //ovde ide stranica
+    }
+    @Test
+    public void userCanGoToLogInPage(){
+
+    }
+}
